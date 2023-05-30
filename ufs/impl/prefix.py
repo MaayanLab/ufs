bc1755ffaeaa2e347e46a57a2a4af35ed9fa5b48
Daniel Clarke <u8sand@gmail.com>
2023-05-30 17:40:57 -0400

    [Prefix]: Helper method for path coersion

--- ufs/impl/prefix.py
+++ ufs/impl/prefix.py
@@ -18,19 +18,21 @@ class Prefix(UFS):
   def __init__(self, ufs: UFS, prefix: str = '/'):
     self._ufs = ufs
     self._prefix = prefix
+  def _path(self, path: str):
+    return self._prefix + str(SafePosixPath(path))[1:]
   def ls(self, path: str) -> list[str]:
     try:
-      return self._ufs.ls(self._prefix + str(SafePosixPath(path))[1:])
+      return self._ufs.ls(self._path(path))
     except FileNotFoundError:
       raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
   def info(self, path: str) -> FileStat:
     try:
-      return self._ufs.info(self._prefix + str(SafePosixPath(path))[1:])
+      return self._ufs.info(self._path(path))
     except FileNotFoundError:
       raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
   def open(self, path: str, mode: t.Literal['rb', 'wb', 'ab', 'rb+', 'ab+']) -> int:
     try:
-      return self._ufs.open(self._prefix + str(SafePosixPath(path))[1:], mode)
+      return self._ufs.open(self._path(path), mode)
     except FileNotFoundError:
       raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
   def seek(self, fd: int, pos: int, whence: t.Literal[0, 1, 2] = 0):
@@ -45,19 +47,19 @@ class Prefix(UFS):
     return self._ufs.close(fd)
   def unlink(self, path: str):
     try:
-      return self._ufs.unlink(self._prefix + str(SafePosixPath(path))[1:])
+      return self._ufs.unlink(self._path(path))
     except FileNotFoundError:
       raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
 
   # optional
   def mkdir(self, path: str):
     try:
-      return self._ufs.mkdir(self._prefix + str(SafePosixPath(path))[1:])
+      return self._ufs.mkdir(self._path(path))
     except FileNotFoundError:
       raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
   def rmdir(self, path: str):
     try:
-      return self._ufs.rmdir(self._prefix + str(SafePosixPath(path))[1:])
+      return self._ufs.rmdir(self._path(path))
     except FileNotFoundError:
       raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
   def flush(self, fd: int):
@@ -66,13 +68,13 @@ class Prefix(UFS):
   # fallback
   def copy(self, src: str, dst: str):
     try:
-      return self._ufs.copy(self._prefix + str(SafePosixPath(src))[1:], self._prefix + str(SafePosixPath(dst))[1:])
+      return self._ufs.copy(self._path(src), self._path(dst))
     except FileNotFoundError as e:
       raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), e.filename.replace(str(self._prefix), ''))
 
   def rename(self, src: str, dst: str):
     try:
-      return self._ufs.rename(self._prefix + str(SafePosixPath(src))[1:], self._prefix + str(SafePosixPath(dst))[1:])
+      return self._ufs.rename(self._path(src), self._path(dst))
     except FileNotFoundError as e:
       raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), e.filename.replace(str(self._prefix), ''))
 
