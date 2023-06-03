a83b2ce1385d6a4a90063fc5a62180bc182b506e
Daniel Clarke <u8sand@gmail.com>
2023-06-03 15:20:12 -0400

    [fsspec]: Also use info cache

--- ufs/impl/fsspec.py
+++ ufs/impl/fsspec.py
@@ -5,6 +5,8 @@ from fsspec.implementations.local import LocalFileSystem
 ufs = FSSpec(LocalFileSystem())
 '''
 import time
+import json
+import fsspec
 import typing as t
 import itertools
 import functools
@@ -13,15 +15,32 @@ from ufs.pathlib import pathparent, pathname
 from datetime import datetime
 from ufs.spec import UFS, FileStat
 
+def fsspec_info_to_ufs_info(info):
+  atime = info.get('atime', time.time())
+  if isinstance(atime, datetime): atime = atime.timestamp()
+  ctime = info.get('ctime', info.get('created', info.get('CreationDate', time.time())))
+  if isinstance(ctime, datetime): ctime = ctime.timestamp()
+  mtime = info.get('mtime', info.get('modified', info.get('LastModified', time.time())))
+  if isinstance(mtime, datetime): mtime = mtime.timestamp()
+  return {
+    'type': info['type'],
+    'size': info['size'],
+    'atime': atime,
+    'ctime': ctime,
+    'mtime': mtime,
+  }
 
 def fsspec_ls(fs, path: str):
   detail = fs.ls(path, detail=True)
   ret = {
-    pathname(item['name']): item
+    pathname(item['name']): fsspec_info_to_ufs_info(item)
     for item in detail
     if pathparent(item['name']) == path
   }
   return ret
+
+def fsspec_info(fs, path: str):
+  return fsspec_info_to_ufs_info(fs.info(path))
 
 class FSSpec(UFS):
   def __init__(self, fs, ttl=60):
@@ -31,6 +50,7 @@ class FSSpec(UFS):
     self._cfd = iter(itertools.count(start=5))
     self._fds = {}
     self._ls_cache = TTLCache(resolve=functools.partial(fsspec_ls, self._fs), ttl=ttl)
+    self._info_cache = TTLCache(resolve=functools.partial(fsspec_info, self._fs), ttl=ttl)
 
   @staticmethod
   def from_dict(*, fs, ttl):
@@ -46,36 +66,23 @@ class FSSpec(UFS):
     )
 
   def _path(self, path: str):
-    return self._fs.root_marker + path[1:]
+    return self._fs.root_marker + path[1:].rstrip('/')
 
   def _ppath(self, path: str):
     return self._path(pathparent(path))
 
   def ls(self, path: str) -> list[str]:
-    return list(self._ls_cache(self._path(path.rstrip('/'))).keys())
+    listing = self._ls_cache(self._path(path))
+    for p, info in listing.items():
+      self._info_cache[p] = info
+    return list(listing.keys())
 
   def info(self, path: str) -> FileStat:
-    try:
-      if pathparent(path) == pathname(path): info = self._fs.info(self._path(path))
-      else: info = self._ls_cache(self._ppath(path))[pathname(path)]
-    except KeyError:
-      raise FileNotFoundError(path)
-    atime = info.get('atime', time.time())
-    if isinstance(atime, datetime): atime = atime.timestamp()
-    ctime = info.get('ctime', info.get('created', info.get('CreationDate', time.time())))
-    if isinstance(ctime, datetime): ctime = ctime.timestamp()
-    mtime = info.get('mtime', info.get('modified', info.get('LastModified', time.time())))
-    if isinstance(mtime, datetime): mtime = mtime.timestamp()
-    return {
-      'type': info['type'],
-      'size': info['size'],
-      'atime': atime,
-      'ctime': ctime,
-      'mtime': mtime,
-    }
+    return self._info_cache(self._path(path))
 
   def open(self, path: str, mode: t.Literal['rb', 'wb', 'ab', 'rb+', 'ab+']) -> int:
     fd = next(self._cfd)
+    self._info_cache.discard(self._path(path))
     self._ls_cache.discard(self._ppath(path))
     self._fds[fd] = (
       path,
@@ -92,18 +99,22 @@ class FSSpec(UFS):
     return self._fds[fd][1].trunate(length)
   def close(self, fd: int):
     path, fh = self._fds.pop(fd)
+    self._info_cache.discard(self._path(path))
     self._ls_cache.discard(self._ppath(path))
     return fh.close()
   def unlink(self, path: str):
+    self._info_cache.discard(self._path(path))
     self._ls_cache.discard(self._ppath(path))
     self._fs.rm_file(self._path(path))
 
   # optional
   def mkdir(self, path: str):
+    self._info_cache.discard(self._path(path))
     self._ls_cache.discard(self._path(path))
     self._ls_cache.discard(self._ppath(path))
     return self._fs.mkdir(self._path(path))
   def rmdir(self, path: str):
+    self._info_cache.discard(self._path(path))
     self._ls_cache.discard(self._path(path))
     self._ls_cache.discard(self._ppath(path))
     return self._fs.rmdir(self._path(path))
@@ -111,11 +122,14 @@ class FSSpec(UFS):
     self._fds[fd][1].flush()
 
   def copy(self, src: str, dst: str):
-    self._ls_cache.discard(self._ppath(dst))
     self._fs.copy(self._path(src), self._path(dst))
+    self._info_cache.discard(self._path(dst))
+    self._ls_cache.discard(self._ppath(dst))
 
   def rename(self, src: str, dst: str):
+    self._info_cache.discard(self._path(src))
     self._ls_cache.discard(self._ppath(src))
+    self._info_cache.discard(self._path(dst))
     self._ls_cache.discard(self._ppath(dst))
     self._fs.rename(self._path(src), self._path(dst))
 
