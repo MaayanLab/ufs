405db7b37e6dfc4712af5de692db4a3d0231f013
Daniel Clarke <u8sand@gmail.com>
2023-06-19 16:25:09 -0400

    [sbfs]: Adopt DescriptorFromAtomic mixin

--- ufs/impl/sbfs.py
+++ ufs/impl/sbfs.py
@@ -1,26 +1,10 @@
 import json
 import aiohttp
-import itertools
-import dataclasses
 import logging
-from ufs.spec import AsyncUFS
+from ufs.spec import AsyncUFS, AsyncDescriptorFromAtomicMixin
 from ufs.utils.pathlib import SafePurePosixPath_
 
 logger = logging.getLogger(__name__)
-
-@dataclasses.dataclass
-class SBFSReadFileDescriptor:
-  path: SafePurePosixPath_
-  download_info: dict
-  pos: int = 0
-
-@dataclasses.dataclass
-class SBFSWriteFileDescriptor:
-  path: SafePurePosixPath_
-  upload_info: dict
-  part: bytes = b''
-  pos: int = 0
-  part_num: int = 1
 
 def one(items):
   it = iter(items)
@@ -30,7 +14,7 @@ def one(items):
   except StopIteration: return item
   else: raise RuntimeError('Expected one, got multiple')
 
-class SBFS(AsyncUFS):
+class SBFS(AsyncDescriptorFromAtomicMixin, AsyncUFS):
   CHUNK_SIZE = 8192
 
   def __init__(self, auth_token: str, api_endpoint='https://cavatica-api.sbgenomics.com'):
@@ -38,8 +22,6 @@ class SBFS(AsyncUFS):
     assert auth_token is not None, 'SBFS auth_token is required'
     self._auth_token = auth_token
     self._api_endpoint = api_endpoint
-    self._cfd = iter(itertools.count(start=5))
-    self._fds = {}
 
   @staticmethod
   def from_dict(*, auth_token, api_endpoint):
@@ -170,106 +152,57 @@ class SBFS(AsyncUFS):
       else:
         raise NotImplementedError(info['type'])
 
-  async def open(self, path, mode, *, size_hint = None):
-    if '+' in mode:
-      raise NotImplementedError(mode)
-    if 'r' in mode:
-      file_info = await self._file_info(path)
-      file_details = await self._file_details(file_info, params=dict(fields='size'))
-      async with self._session.get(f"{self._api_endpoint}/v2/files/{file_info['id']}/download_info") as req:
-        download_info = await req.json()
-      descriptor = SBFSReadFileDescriptor(path, dict(download_info, size=file_details['size']))
-      fd = next(self._cfd)
-      self._fds[fd] = descriptor
-    elif 'w' in mode:
-      if not size_hint: raise NotImplementedError('no sizehint write')
-      async with self._session.post(f"{self._api_endpoint}/v2/upload/multipart", params=dict(overwrite='true'), json=dict(
-        await self._as_parent_params(path.parent),
-        name=path.name,
-        size=size_hint,
-        part_size=min(1073741824, size_hint),
-      )) as req:
-        upload_info = await req.json()
-      descriptor = SBFSWriteFileDescriptor(path, upload_info)
-      fd = next(self._cfd)
-      self._fds[fd] = descriptor
+  async def cat(self, path):
+    file_info = await self._file_info(path)
+    async with self._session.get(f"{self._api_endpoint}/v2/files/{file_info['id']}/download_info") as req:
+      download_info = await req.json()
+    async with self._session.get(download_info['url']) as req:
+      async for chunk, _ in req.content.iter_chunks():
+        yield chunk
+
+  async def put(self, path, data, *, size_hint=None):
+    if not size_hint: raise NotImplementedError('no sizehint write')
+    async with self._session.post(f"{self._api_endpoint}/v2/upload/multipart", params=dict(overwrite='true'), json=dict(
+      await self._as_parent_params(path.parent),
+      name=path.name,
+      size=size_hint,
+      part_size=min(1073741824, size_hint),
+    )) as req:
+      upload_info = await req.json()
+    try:
+      part = b''
+      pos = 0
+      part_number = 1
+
+      async for buf in data:
+        part += buf
+
+        while part and (len(part) >= upload_info['part_size'] or pos + len(part) >= size_hint):
+          part = part[:upload_info['part_size']]
+          async with self._session.get(f"{self._api_endpoint}/v2/upload/multipart/{upload_info['upload_id']}/part/{part_number}") as req:
+            part_info = await req.json()
+            logger.debug(part_info)
+          async with self._session.request(part_info['method'], part_info['url'], data=part, headers=part_info['headers']) as req:
+            logger.debug(await req.read())
+            upload_response = {'headers': {k: json.loads(req.headers.get(k)) for k in part_info['report']['headers']}}
+          async with self._session.post(f"{self._api_endpoint}/v2/upload/multipart/{upload_info['upload_id']}/part", json=dict(
+            part_number=part_number,
+            response=upload_response,
+          )) as req:
+            logger.debug(await req.read())
+          pos += len(part)
+          part = part[pos:]
+          part_number += 1
+
+      assert pos == size_hint, f"{pos} != {size_hint}"
+    except:
+      logger.debug(f"cancel")
+      async with self._session.delete(f"{self._api_endpoint}/v2/upload/multipart/{upload_info['upload_id']}") as req:
+        logger.debug(await req.read())
     else:
-      raise NotImplementedError(mode)
-    return fd
-  
-  async def seek(self, fd, pos, whence = 0):
-    if whence != 0: raise NotImplementedError()
-    if isinstance(self._fds[fd], SBFSWriteFileDescriptor) and pos != self._fds[fd].pos:
-      raise NotImplementedError()
-    self._fds[fd].pos = pos
-
-  async def read(self, fd, amnt = -1):
-    descriptor = self._fds[fd]
-    if isinstance(descriptor, SBFSWriteFileDescriptor):
-      raise RuntimeError()
-    if amnt == 0:
-      ret = b''
-    elif amnt > 0:
-      start = descriptor.pos
-      end = min(descriptor.download_info['size'], descriptor.pos + amnt)
-      if start == end:
-        ret = b''
-      else:
-        logger.debug(f"{descriptor.download_info['url']} bytes={start}-{end}")
-        async with self._session.get(descriptor.download_info['url'], headers={
-          'Range': f"bytes={start}-{end}"
-        }) as req:
-          ret = await req.content.read()
-          descriptor.pos += len(ret)
-    else:
-      async with self._session.get(descriptor.download_info['url']) as req:
-        ret = await req.content.read()
-        descriptor.pos += len(ret)
-    return ret
-
-  async def write(self, fd, data):
-    descriptor = self._fds[fd]
-    if isinstance(descriptor, SBFSReadFileDescriptor):
-      raise RuntimeError()
-    descriptor.part += data
-    return len(data)
-
-  async def truncate(self, fd, length):
-    raise NotImplementedError()
-
-  async def flush(self, fd: int):
-    descriptor = self._fds[fd]
-    if isinstance(descriptor, SBFSWriteFileDescriptor):
-      while descriptor.part and (len(descriptor.part) >= descriptor.upload_info['part_size'] or descriptor.pos + len(descriptor.part) >= descriptor.upload_info['size']):
-        part = descriptor.part[:descriptor.upload_info['part_size']]
-        async with self._session.get(f"{self._api_endpoint}/v2/upload/multipart/{descriptor.upload_info['upload_id']}/part/{descriptor.part_num}") as req:
-          part_info = await req.json()
-          logger.debug(part_info)
-        async with self._session.request(part_info['method'], part_info['url'], data=part, headers=part_info['headers']) as req:
-          logger.debug(await req.read())
-          upload_response = {'headers': {k: json.loads(req.headers.get(k)) for k in part_info['report']['headers']}}
-        async with self._session.post(f"{self._api_endpoint}/v2/upload/multipart/{descriptor.upload_info['upload_id']}/part", json=dict(
-          part_number=descriptor.part_num,
-          response=upload_response,
-        )) as req:
-          logger.debug(await req.read())
-        descriptor.pos += len(part)
-        descriptor.part = descriptor.part[descriptor.pos:]
-        descriptor.part_num += 1
-
-  async def close(self, fd):
-    await self.flush(fd)
-    descriptor = self._fds.pop(fd)
-    if isinstance(descriptor, SBFSWriteFileDescriptor):
-      logger.debug(f"{descriptor=}")
-      if descriptor.pos == descriptor.upload_info['size']:
-        logger.debug(f"complete")
-        async with self._session.post(f"{self._api_endpoint}/v2/upload/multipart/{descriptor.upload_info['upload_id']}/complete") as req:
-          logger.debug(await req.read())
-      else:
-        logger.debug(f"cancel")
-        async with self._session.delete(f"{self._api_endpoint}/v2/upload/multipart/{descriptor.upload_info['upload_id']}") as req:
-          logger.debug(await req.read())
+      logger.debug(f"complete")
+      async with self._session.post(f"{self._api_endpoint}/v2/upload/multipart/{upload_info['upload_id']}/complete") as req:
+        logger.debug(await req.read())
 
   async def unlink(self, path):
     info = await self._file_info(path)
