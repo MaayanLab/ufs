300bbf9f89beed4292b2b7cd1dd05eb969d37aa1
Daniel Clarke <u8sand@gmail.com>
2023-06-22 18:18:30 -0400

    [url]: WIP untested

--- ufs/url.py
+++ ufs/url.py
@@ -1,6 +1,5 @@
-import functools
 import contextlib
-from ufs.utils.url import parse_url, parse_qs
+from ufs.utils.url import parse_url, parse_netloc, parse_fragment_qs
 from ufs.utils.pathlib import SafePurePosixPath
 
 protos = {}
@@ -10,57 +9,78 @@ def register_proto_handler(proto):
     return func
   return decorator
 
-@functools.lru_cache()
-def ufs_local():
-  from ufs.impl.local import Local
-  return Local()
-
 @register_proto_handler(None)
 @register_proto_handler('file')
 def proto_file(url):
+  from ufs.impl.local import Local
   from ufs.impl.prefix import Prefix
-  return Prefix(ufs_local(), url['path'])
-
-@functools.lru_cache()
-def ufs_memory():
-  from ufs.impl.memory import Memory
-  return Memory()
+  return Prefix(Local(), url['path'])
 
 @register_proto_handler('memory')
 def proto_memory(url):
+  from ufs.impl.memory import Memory
   from ufs.impl.prefix import Prefix
-  return Prefix(ufs_memory(), url['path'])
-
-@functools.lru_cache()
-def ufs_rclone(**kwargs):
-  from ufs.impl.rclone import RClone
-  return RClone(**kwargs)
+  return Prefix(Memory(), url['path'])
 
 @register_proto_handler('rclone')
 def proto_rclone(url):
+  from ufs.impl.rclone import RClone
   from ufs.impl.prefix import Prefix
-  return Prefix(ufs_rclone(**parse_qs(url)), url['path'])
-
-@functools.lru_cache()
-def ufs_s3(**kwargs):
-  from ufs.impl.s3 import S3
-  return S3(**kwargs)
+  return Prefix(RClone(**parse_fragment_qs(url)), url['path'])
 
 @register_proto_handler('s3')
 def proto_s3(url):
+  from ufs.impl.s3 import S3
   from ufs.impl.prefix import Prefix
-  return Prefix(ufs_s3(**parse_qs(url)), url['path'])
-
-@functools.lru_cache()
-def ufs_sbfs(**kwargs):
-  from ufs.impl.sbfs import SBFS
-  from ufs.impl.sync import Sync
-  return Sync(SBFS(**kwargs))
+  return Prefix(S3(**parse_fragment_qs(url)), url['path'])
 
 @register_proto_handler('sbfs')
 def proto_sbfs(url):
   from ufs.impl.prefix import Prefix
-  return Prefix(ufs_sbfs(**parse_qs(url)), url['path'])
+  from ufs.impl.sbfs import SBFS
+  from ufs.impl.sync import Sync
+  return Prefix(Sync(SBFS(**parse_fragment_qs(url))), url['path'])
+
+@register_proto_handler('ftps')
+@register_proto_handler('ftp')
+def proto_ftp(url):
+  from ufs.impl.ftp import FTP
+  from ufs.impl.prefix import Prefix
+  netloc_parsed = parse_netloc(url)
+  return Prefix(
+    FTP(
+      netloc_parsed['host'],
+      user=netloc_parsed.get('username'),
+      passwd=netloc_parsed.get('password'),
+      tls=url['proto'] == 'ftps',
+      **parse_fragment_qs(url)
+    ),
+    netloc_parsed['path']
+  )
+
+@register_proto_handler('https')
+@register_proto_handler('http')
+def proto_http(url):
+  from ufs.impl.http import HTTP
+  from ufs.impl.prefix import Prefix
+  netloc_parsed = parse_netloc(url)
+  return Prefix(
+    HTTP(
+      netloc_parsed['netloc'],
+      scheme=url['proto'],
+      **parse_fragment_qs(url)
+    ),
+    netloc_parsed['path']
+  )
+
+@register_proto_handler('drs')
+def proto_drs(url):
+  from ufs.impl.drs import DRS
+  from ufs.impl.prefix import Prefix
+  return Prefix(
+    DRS(),
+    url['path']
+  )
 
 def ufs_from_url(url: str, protos=protos):
   '''
@@ -76,7 +96,7 @@ def ufs_from_url(url: str, protos=protos):
 def open_from_url(url: str, mode='r', protos=protos):
   '''
   Usage:
-  with open_from_url('s3://mybucket/myprefix/my_file.tsv?anon=true') as fr:
+  with open_from_url('s3://mybucket/myprefix/my_file.tsv#?anon=true') as fr:
     print(fr.read())
   '''
   url_parsed = parse_url(url)
--- ufs/utils/url.py
+++ ufs/utils/url.py
@@ -6,20 +6,50 @@ from ufs.utils.one import one
 class URLParsed(t.TypedDict):
   proto: t.Optional[str]
   path: str
-  qs: t.Optional[str]
   fragment: t.Optional[str]
 
-url_expr = re.compile(r'((?P<proto>[^:]+)://)?(?P<path>[^\?#]*)(\?(?P<qs>[^#]*))?(#(?P<fragment>.*))?')
+url_expr = re.compile(r'((?P<proto>[^:]+)://)?(?P<path>[^#]*)(#(?P<fragment>.*))?')
 
 def parse_url(url) -> URLParsed:
+  ''' We parse the url, separating protocol, path, fragment and a query string in the fragment
+   (proto:///some/path#fragment_section)
+  We ignore the regular query string since it could be used by `http` for instance. Fragments however
+   are invalid as server-side urls since they are browser side only, thus we will use qs notation in
+   the fragment section of the url for options overrides.
+  '''
   m = url_expr.match(url)
   if not m: raise RuntimeError(f"Invalid url: {url}")
   return m.groupdict()
+
+
+class NetlocParsed(t.TypedDict):
+  username: t.Optional[str]
+  password: t.Optional[str]
+  host: t.Optional[str]
+  port: t.Optional[int]
+  netloc: str
+  path: str
+
+netloc_expr = re.compile(r'((?P<username>[^:]+)?(P<password>[^@]+)?@)?(?P<host>[^:/]+)(:(?P<port>\d+))?')
+
+def parse_netloc(url_parsed: URLParsed) -> NetlocParsed:
+  netloc, _, path = url_parsed['path'].partition('/')
+  m = netloc_expr.match(netloc)
+  if not m: raise RuntimeError(f"Invalid netloc: {netloc}")
+  netloc_parsed = m.groupdict()
+  return dict(
+    netloc_parsed,
+    netloc=netloc,
+    port=int(netloc_parsed['port']) if netloc_parsed.get('port') else None,
+    path='/'+(path or ''),
+  )
+
 
 def try_json_loads(s):
   try: return json.loads(s)
   except: return s
 
-def parse_qs(url_parsed: URLParsed):
+def parse_fragment_qs(url_parsed: URLParsed):
   from urllib.parse import parse_qs
-  return { k: try_json_loads(one(v)) for k, v in parse_qs(url_parsed['qs'] or '').items() }
+  _, _, qs = (url_parsed.get('fragment') or '').partition('?')
+  return { k: try_json_loads(one(v)) for k, v in parse_qs(qs or '').items() }
