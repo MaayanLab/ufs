843594dbec3fc70359f8fd6962ec81bf44c80425
Daniel Clarke <u8sand@gmail.com>
2023-06-23 12:40:38 -0400

    [ftp]: Working impl

--- ufs/impl/ftp.py
+++ ufs/impl/ftp.py
@@ -1,7 +1,39 @@
+import queue
 import ftplib
+import itertools
+import contextlib
 import threading as t
 from ufs.spec import DescriptorFromAtomicMixin, UFS, QueuedIterator, ReadableIterator
 from ufs.utils.one import one
+
+def ftp_client_thread(send: queue.Queue, recv: queue.Queue, opts: dict):
+  ftp = ftplib.FTP_TLS() if opts['tls'] else ftplib.FTP()
+  ftp.connect(opts['host'], opts['port'])
+  ftp.login(opts['user'], opts['passwd'])
+  while True:
+    msg = recv.get()
+    i, op, args, kwargs = msg
+    if op == None:
+      recv.task_done()
+      break
+    try:
+      func = getattr(ftp, op)
+      if op in {'retrlines', 'retrbinary'}:
+        cmd, stream = args
+        res = func(cmd, stream.write, **kwargs)
+      elif op in {'storbinary'}:
+        cmd, stream = args
+        res = func(cmd, ReadableIterator(iter(stream.get, None)), **kwargs)
+      else:
+        res = func(*args, **kwargs)
+    except Exception as err:
+      send.put([i, None, err])
+    else:
+      send.put([i, res, None])
+    finally:
+      if op in {'retrlines', 'retrbinary'}: stream.close()
+    recv.task_done()
+  ftp.quit()
 
 class FTP(DescriptorFromAtomicMixin, UFS):
   def __init__(self, host: str, user = '', passwd = '', port = 21, tls = False) -> None:
@@ -11,6 +43,7 @@ class FTP(DescriptorFromAtomicMixin, UFS):
     self._passwd = passwd
     self._port = port
     self._tls = tls
+    self._taskid = iter(itertools.count())
 
   @staticmethod
   def from_dict(*, host, user, passwd, port, tls):
@@ -31,49 +64,118 @@ class FTP(DescriptorFromAtomicMixin, UFS):
       tls=self._tls,
     )
 
+  @contextlib.contextmanager
+  def _translate_exc(self):
+    try:
+      yield
+    except ftplib.error_perm as e:
+      if 'No such file or directory.' in str(e):
+        raise FileNotFoundError(e)
+      elif 'File exists.' in str(e):
+        raise FileExistsError(e)
+      else:
+        raise e
+
+  def _forward(self, op, *args, **kwargs):
+    self.start()
+    i, i_ = next(self._taskid), None
+    self._send.put([i, op, args, kwargs])
+    while True:
+      i_, ret, err = self._recv.get()
+      self._recv.task_done()
+      if i == i_:
+        break
+      # a different result came before ours, add it back to the queue and try again
+      self._recv.put([i_, ret, err])
+    if err is not None:
+      with self._translate_exc():
+        raise err
+    else: return ret
+
+  @contextlib.contextmanager
+  def _forward_queue(self, op, *args, **kwargs):
+    self.start()
+    i, i_ = next(self._taskid), None
+    self._send.put([i, op, args, kwargs])
+    yield
+    while True:
+      i_, ret, err = self._recv.get()
+      self._recv.task_done()
+      if i == i_:
+        break
+      # a different result came before ours, add it back to the queue and try again
+      self._recv.put([i_, ret, err])
+    if err is not None:
+      with self._translate_exc():
+        raise err
+
   def start(self):
-    if not hasattr(self, '_ftp'):
-      self._ftp = ftplib.FTP_TLS() if self._tls else ftplib.FTP()
-      self._ftp.connect(self._host, self._port)
-      self._ftp.login(self._user, self._passwd)
+    if not hasattr(self, '_ftp_client_thread'):
+      self._send, self._recv = queue.Queue(), queue.Queue()
+      self._ftp_client_thread = t.Thread(
+        target=ftp_client_thread,
+        args=(self._recv, self._send, self.to_dict()),
+      )
+      self._ftp_client_thread.start()
   
   def stop(self):
-    if hasattr(self, '_ftp'):
-      self._ftp.quit()
-      del self._ftp
+    if hasattr(self, '_ftp_client_thread'):
+      self._send.put([next(iter(self._taskid)), None, None, None])
+      self._ftp_client_thread.join()
+      del self._ftp_client_thread
 
   def ls(self, path):
-    return self._ftp.nlst(str(path))
-  
+    return self._forward('nlst', str(path))
+
   def info(self, path):
-    facts = one(
-      facts
-      for file, facts in self._ftp.mlsd(str(path.parent), ['type', 'size'])
-      if file == path.name
-    )
-    if facts['type'] == 'dir':
+    if str(path) == '/': return { 'type': 'directory', 'size': 0 }
+    stream = QueuedIterator()
+    with self._forward_queue('retrlines', 'LIST ' + str(path.parent), stream):
+      try:
+        info = one(
+          {
+            # the first character (part of the permissions bits) will be '-' for a file and 'd' for a directory
+            'type': line[0],
+            # the 5th field contains the size
+            'size': int(line_split[4]),
+            # right after that is the date until the filename
+            'time': ' '.join(line_split[5:7]),
+          }
+          for line in stream
+          for line_split in (line.split(maxsplit=9),)
+          # the last field will be the filename
+          if line_split[-1] == path.name
+        )
+      except StopIteration:
+        raise FileNotFoundError(path)
+    if info['type'] == 'd':
       return { 'type': 'directory', 'size': 0 }
     else:
-      return { 'type': 'file', 'size': facts['size'] }
+      return { 'type': 'file', 'size': info['size'] }
 
   def cat(self, path):
-    queued_iterator = QueuedIterator()
-    thread = t.Thread(target=self._ftp.retrbinary, args=('RETR ' + str(path), queued_iterator.write,))
-    thread.start()
-    yield from queued_iterator
-    thread.join()
+    stream = QueuedIterator()
+    with self._forward_queue('retrbinary', 'RETR ' + str(path), stream):
+      yield from stream
 
   def put(self, path, data, *, size_hint = None):
-    self._ftp.storbinary('STOR ' + str(path), ReadableIterator(data))
+    stream = queue.Queue()
+    try:
+      with self._forward_queue('storbinary', 'STOR ' + str(path), stream):
+        for chunk in data:
+          stream.put(chunk)
+        stream.put(None)
+    except ftplib.error_perm:
+      raise PermissionError(str(path))
 
   def unlink(self, path):
-    self._ftp.delete(str(path))
+    self._forward('delete', str(path))
 
   def mkdir(self, path):
-    self._ftp.mkd(str(path))
+    self._forward('mkd', str(path))
 
   def rmdir(self, path):
-    self._ftp.rmd(str(path))
+    self._forward('rmd', str(path))
 
   def rename(self, src, dst):
-    self._ftp.rename(str(src), str(dst))
+    self._forward('rename', str(src), str(dst))
