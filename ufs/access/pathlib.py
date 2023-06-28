fbc5e1be06911143bd756cfb2c7c20c80faea8a5
Daniel Clarke <u8sand@gmail.com>
2023-06-28 17:00:25 -0400

    [ufs.access.pathlib]: Added AsyncUPath

--- ufs/access/pathlib.py
+++ ufs/access/pathlib.py
@@ -1,8 +1,7 @@
 ''' Implement a pathlib.Path-like interface to UFS
 '''
-import pathlib
-from ufs.spec import UFS
-from ufs.utils.pathlib import SafePurePosixPath, SafePurePosixPath_, PathLike
+from ufs.spec import UFS, AsyncUFS
+from ufs.utils.pathlib import SafePurePosixPath, PathLike
 
 class UPath:
   ''' A class implementing `pathlib.Path` methods for a `ufs`
@@ -48,8 +47,12 @@ class UPath:
       return False
 
   def open(self, mode: str, *, size_hint = None):
-    if 'b' in mode: return UPathBinaryOpener(self._ufs, self._path, mode, size_hint=size_hint)
-    else: return UPathOpener(self._ufs, self._path, mode, size_hint=size_hint)
+    if 'b' in mode:
+      return UPathBinaryOpener(self._ufs, self._ufs.open(self._path, mode, size_hint=size_hint))
+    else:
+      if mode.endswith('+'): mode_ = mode[:-1] + 'b+'
+      else: mode_ = mode + 'b'
+      return UPathOpener(self._ufs, self._ufs.open(self._path, mode_, size_hint=size_hint))
 
   def unlink(self):
     self._ufs.unlink(self._path)
@@ -92,11 +95,9 @@ class UPath:
       fw.write(text)
 
 class UPathBinaryOpener:
-  def __init__(self, ufs: UFS, path: SafePurePosixPath_, mode: str, *, size_hint: int=None):
+  def __init__(self, ufs: UFS, fd: int):
     self._ufs = ufs
-    self._path = path
-    self._mode = mode
-    self._fd = self._ufs.open(self._path, self._mode, size_hint=size_hint)
+    self._fd = fd
     self.closed = False
   def __enter__(self):
     return self
@@ -116,22 +117,127 @@ class UPathBinaryOpener:
     return self._ufs.write(self._fd, data)
 
 class UPathOpener(UPathBinaryOpener):
-  def __init__(self, ufs: UFS, path: SafePurePosixPath_, mode: str, *, size_hint: int=None):
-    if mode.endswith('+'): mode_ = mode[:-1] + 'b+'
-    else: mode_ = mode + 'b'
-    super().__init__(ufs, path, mode_, size_hint=size_hint)
   def write(self, data: str) -> int:
     return super().write(data.encode('utf-8'))
   def read(self, amnt: int = -1) -> str:
     return super().read(amnt).decode('utf-8')
 
-def rmtree(upath: UPath | pathlib.Path):
-  ''' This doesn't exist in normal pathlib but comes in handy
+
+class AsyncUPath:
+  ''' A class implementing `pathlib.Path` methods for a `ufs`
   '''
-  Q = [(upath, True)] + [(path, False) for path in upath.iterdir()]
-  while Q:
-    path, empty = Q.pop()
-    if path.is_file(): path.unlink()
-    elif path.is_dir():
-      if empty: path.rmdir()
-      else: Q += [(path, True)] + [(pp, False) for pp in path.iterdir()]
+  def __init__(self, ufs: AsyncUFS, path: PathLike = '/') -> None:
+    self._ufs = ufs
+    self._path = SafePurePosixPath(path)
+  
+  @property
+  def name(self):
+    return self._path.name
+
+  @property
+  def parent(self):
+    return AsyncUPath(self._ufs, self._path.parent)
+
+  def __str__(self):
+    return str(self._path)
+
+  def __repr__(self) -> str:
+    return f"AsyncUPath({repr(self._ufs)}, {repr(self._path)})"
+
+  def __truediv__(self, subpath: PathLike):
+    return self.__class__(self._ufs, self._path / subpath)
+
+  async def exists(self):
+    try:
+      await self._ufs.info(self._path)
+      return True
+    except FileNotFoundError:
+      return False
+
+  async def is_file(self):
+    try:
+      return (await self._ufs.info(self._path)['type']) == 'file'
+    except FileNotFoundError:
+      return False
+
+  async def is_dir(self):
+    try:
+      return (await self._ufs.info(self._path)['type']) == 'directory'
+    except FileNotFoundError:
+      return False
+
+  async def open(self, mode: str, *, size_hint = None):
+    if 'b' in mode:
+      return AsyncUPathBinaryOpener(self._ufs, await self._ufs.open(self._path, mode, size_hint=size_hint))
+    else:
+      if mode.endswith('+'): mode_ = mode[:-1] + 'b+'
+      else: mode_ = mode + 'b'
+      return AsyncUPathOpener(self._ufs, await self._ufs.open(self._path, mode_, size_hint=size_hint))
+
+  async def unlink(self):
+    await self._ufs.unlink(self._path)
+
+  async def mkdir(self, parents=False, exist_ok=False):
+    try:
+      if parents:
+        if not await self.parent.exists():
+          await self.parent.mkdir(parents=True)
+      await self._ufs.mkdir(self._path)
+    except FileExistsError as e:
+      if not exist_ok: raise e
+
+  async def rmdir(self):
+    await self._ufs.rmdir(self._path)
+
+  async def rename(self, other: str):
+    if str(other).startswith('/'):
+      await self._ufs.rename(self._path, SafePurePosixPath(other))
+    else:
+      await self._ufs.rename(self._path, self._path.parent/other)
+
+  async def iterdir(self):
+    for name in await self._ufs.ls(self._path):
+      yield self / name
+  
+  async def read_text(self):
+    async with await self.open('r') as fr:
+      return await fr.read()
+
+  async def write_text(self, text: str):
+    await self.write_bytes(text.encode())
+
+  async def read_bytes(self) -> bytes:
+    async with await self.open('rb') as fr:
+      return await fr.read()
+
+  async def write_bytes(self, text: bytes):
+    async with await self.open('wb', size_hint=len(text)) as fw:
+      await fw.write(text)
+
+class AsyncUPathBinaryOpener:
+  def __init__(self, ufs: AsyncUFS, fd: int):
+    self._ufs = ufs
+    self._fd = fd
+    self.closed = False
+  async def __aenter__(self):
+    return self
+  async def __aexit__(self, *args):
+    await self.close()
+  async def close(self):
+    self.closed = True
+    await self._ufs.close(self._fd)
+  async def seek(self, amnt: int, whence: int = 0):
+    assert not self.closed
+    return await self._ufs.seek(self._fd, amnt, whence)
+  async def read(self, amnt: int = -1) -> bytes:
+    assert not self.closed
+    return await self._ufs.read(self._fd, amnt)
+  async def write(self, data: bytes) -> int:
+    assert not self.closed
+    return await self._ufs.write(self._fd, data)
+
+class AsyncUPathOpener(UPathBinaryOpener):
+  async def write(self, data: str) -> int:
+    return await super().write(data.encode('utf-8'))
+  async def read(self, amnt: int = -1) -> str:
+    return (await super().read(amnt)).decode('utf-8')
