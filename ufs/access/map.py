11d251ccfb8891e5d2954226e5b91f2be8297eac
Daniel Clarke <u8sand@gmail.com>
2023-06-29 19:19:19 -0400

    [access.map]: urlencode keys

--- ufs/access/map.py
+++ ufs/access/map.py
@@ -2,6 +2,7 @@
 '''
 
 import typing as t
+import urllib.parse
 from collections.abc import Mapping, MutableMapping
 from ufs.spec import UFS
 from ufs.access.pathlib import UPath
@@ -18,38 +19,45 @@ class UMap(MutableMapping):
     })})'''
 
   def __getitem__(self, key: str) -> t.Union[str, 'UMap']:
-    if (self._upath/key).is_file():
-      return (self._upath/key).read_text()
-    elif (self._upath/key).is_dir():
-      return UMap(upath=self._upath/key)
+    name = urllib.parse.quote(str(key), safe='')
+    if (self._upath/name).is_file():
+      return (self._upath/name).read_text()
+    elif (self._upath/name).is_dir():
+      return UMap(upath=self._upath/name)
     else:
       raise KeyError(key)
 
   def __setitem__(self, key: str, value: t.Union[str, Mapping]):
-    if (self._upath/key).exists():
-      if (self._upath/key).is_file(): (self._upath/key).unlink()
-      elif (self._upath/key).is_dir(): rmtree(self._upath/key)
+    name = urllib.parse.quote(str(key), safe='')
+    if (self._upath/name).exists():
+      if (self._upath/name).is_file(): (self._upath/name).unlink()
+      elif (self._upath/name).is_dir(): rmtree(self._upath/name)
     if type(value) == str:
-      (self._upath/key).write_text(value)
+      (self._upath/name).write_text(value)
     elif isinstance(value, Mapping):
-      (self._upath/key).mkdir()
-      submap = UMap(upath=self._upath/key)
+      (self._upath/name).mkdir()
+      submap = UMap(upath=self._upath/name)
       for k, v in value.items():
         submap[k] = v
     else:
       raise NotImplementedError(value)
 
   def __delitem__(self, key: str) -> None:
-    if (self._upath/key).is_file():
-      return (self._upath/key).unlink()
-    elif (self._upath/key).is_dir():
-      return rmtree(self._upath/key)
+    name = urllib.parse.quote(str(key), safe='')
+    if (self._upath/name).is_file():
+      return (self._upath/name).unlink()
+    elif (self._upath/name).is_dir():
+      return rmtree(self._upath/name)
     else:
       raise KeyError(key)
 
   def __iter__(self):
     for item in self._upath.iterdir():
-      yield item.name
+      yield urllib.parse.unquote(item.name)
+
+  def __contains__(self, key: str):
+    name = urllib.parse.quote(str(key), safe='')
+    return (self._upath/name).exists()
 
   def __len__(self) -> int:
     return sum(1 for _ in self._upath.iterdir())
