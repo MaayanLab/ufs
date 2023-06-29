f0cf5f750891aebfe91211917f22946dd582a73c
Daniel Clarke <u8sand@gmail.com>
2023-06-29 12:29:52 -0400

    [impl.overlay]: Added overlay ufs and tests

---
+++ ufs/impl/overlay.py
@@ -0,0 +1,126 @@
+import logging
+import itertools
+from ufs.spec import UFS
+
+logger = logging.getLogger(__name__)
+
+class Overlay(UFS):
+  def __init__(self, lower: UFS, upper: UFS):
+    '''
+    lower: fallback read from here
+    upper: writes go here
+    '''
+    super().__init__()
+    self._lower = lower
+    self._upper = upper
+    self._fds = {}
+    self._cfd = iter(itertools.count())
+
+  @staticmethod
+  def from_dict(*, lower, upper):
+    return Overlay(
+      lower=UFS.from_dict(**lower),
+      upper=UFS.from_dict(**upper),
+    )
+
+  def to_dict(self):
+    return dict(super().to_dict(),
+      lower=self._lower.to_dict(),
+      upper=self._upper.to_dict(),
+    )
+
+  def ls(self, path):
+    try:
+      upper_files = self._upper.ls(path)
+    except FileNotFoundError:
+      upper_files = None
+    try:
+      lower_files = self._lower.ls(path)
+    except FileNotFoundError:
+      lower_files = None
+    if upper_files is None and lower_files is None:
+      raise FileNotFoundError(path)
+    return (upper_files or []) + (lower_files or [])
+
+  def info(self, path):
+    try:
+      return self._upper.info(path)
+    except FileNotFoundError:
+      return self._lower.info(path)
+
+  def open(self, path, mode, *, size_hint = None):
+    if 'r' in mode and '+' not in mode:
+      try:
+        provider, fd = self._upper, self._upper.open(path, mode, size_hint=size_hint)
+      except FileNotFoundError:
+        provider, fd = self._lower, self._lower.open(path, mode, size_hint=size_hint)
+    elif '+' in mode:
+      try:
+        provider, fd = self._upper, self._upper.open(path, mode, size_hint=size_hint)
+      except FileNotFoundError:
+        from ufs.access.shutil import copyfile
+        copyfile(self._lower, path, self._upper, path)
+        provider, fd = self._upper, self._upper.open(path, mode, size_hint=size_hint)
+    provider, fd = self._upper, self._upper.open(path, mode, size_hint=size_hint)
+    cfd = next(self._cfd)
+    self._fds[cfd] = (provider, fd)
+    return cfd
+
+  def seek(self, fd, pos, whence = 0):
+    provider, fd = self._fds[fd]
+    return provider.seek(fd, pos, whence)
+
+  def read(self, fd, amnt):
+    provider, fd = self._fds[fd]
+    return provider.read(fd, amnt)
+
+  def write(self, fd, data):
+    provider, fd = self._fds[fd]
+    assert provider is self._upper
+    return provider.write(fd, data)
+
+  def truncate(self, fd, length):
+    provider, fd = self._fds[fd]
+    return provider.truncate(fd, length)
+
+  def flush(self, fd):
+    provider, fd = self._fds[fd]
+    return provider.flush(fd)
+
+  def close(self, fd):
+    provider, fd = self._fds.pop(fd)
+    return provider.close(fd)
+
+  def unlink(self, path):
+    return self._upper.unlink(path)
+
+  def mkdir(self, path):
+    return self._upper.mkdir(path)
+
+  def rmdir(self, path):
+    return self._upper.rmdir(path)
+
+  def copy(self, src, dst):
+    try:
+      if self._upper.info(src)['type'] == 'file':
+        self._upper.copy(src, dst)
+      else:
+        raise IsADirectoryError(src)
+    except FileNotFoundError:
+      from ufs.access.shutil import copyfile
+      copyfile(self._lower, src, self._upper, dst)
+
+  def rename(self, src, dst):
+    try:
+      return self._upper.rename(src, dst)
+    except FileNotFoundError:
+      if self._lower.info(src)['type'] == 'file':
+        raise PermissionError()
+
+  def start(self):
+    self._lower.start()
+    self._upper.start()
+
+  def stop(self):
+    self._upper.stop()
+    self._lower.stop()
--- ufs/tests/test_ufs.py
+++ ufs/tests/test_ufs.py
@@ -10,6 +10,7 @@ from ufs.utils.pathlib import SafePurePosixPath
   'local-memory-writecache',
   'memory-async-sync',
   'tempdir',
+  'overlay',
   'fsspec-local',
   'fsspec-memory',
   'dircache-local',
@@ -55,6 +56,11 @@ def ufs(request):
   elif request.param == 'tempdir':
     from ufs.impl.tempdir import TemporaryDirectory
     with TemporaryDirectory() as ufs:
+      yield ufs
+  elif request.param == 'overlay':
+    from ufs.impl.overlay import Overlay
+    from ufs.impl.memory import Memory
+    with Overlay(Memory(), Memory()) as ufs:
       yield ufs
   elif request.param == 'fsspec-local':
     import tempfile
@@ -323,3 +329,24 @@ def test_ufs(path: UPath):
   assert [p.name for p in path.iterdir()] == ['B']
   (path/'B').unlink()
   assert not (path/'B').exists()
+
+def test_overlay():
+  from ufs.impl.memory import Memory
+  from ufs.impl.overlay import Overlay
+  from ufs.access.pathlib import UPath
+  lower = Memory()
+  upper = Memory()
+  with Overlay(lower, upper) as overlay:
+    lower = UPath(lower)
+    upper = UPath(upper)
+    overlay = UPath(overlay)
+    (overlay / 'test_dir').mkdir()
+    (lower / 'test').write_text('Hello World')
+    assert {p.name for p in overlay.iterdir()} == {'test', 'test_dir'}
+    (overlay / 'test2').write_text('Hello World!')
+    assert {p.name for p in lower.iterdir()} == {'test'}
+    assert {p.name for p in upper.iterdir()} == {'test2', 'test_dir'}
+    assert {p.name for p in overlay.iterdir()} == {'test', 'test2', 'test_dir'}
+    (overlay / 'test').write_text('Hello World!')
+    assert (lower / 'test').read_text() == 'Hello World'
+    assert (overlay / 'test').read_text() == 'Hello World!'
