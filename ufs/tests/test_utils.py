3e92d67e4d2a9cf99e0b394e446d9fb46f2c9737
Daniel Clarke <u8sand@gmail.com>
2023-06-29 20:44:17 -0400

    Move test_url_parse into test_utils

--- ufs/tests/test_utils.py
+++ ufs/tests/test_utils.py
@@ -1,3 +1,36 @@
+def test_url_parse():
+  from ufs.utils.url import parse_url, parse_netloc
+  url_parsed = parse_url('https://user:pass@host:1000/path#?test=true')
+  assert url_parsed == dict(
+    proto='https',
+    path='user:pass@host:1000/path',
+    fragment='?test=true',
+  )
+  netloc_parsed = parse_netloc(url_parsed)
+  assert netloc_parsed == dict(
+    netloc='user:pass@host:1000',
+    username='user',
+    password='pass',
+    host='host',
+    port=1000,
+    path='/path',
+  )
+  url_parsed = parse_url('https://user@host:1000')
+  assert url_parsed == dict(
+    proto='https',
+    path='user@host:1000',
+    fragment=None,
+  )
+  netloc_parsed = parse_netloc(url_parsed)
+  assert netloc_parsed == dict(
+    netloc='user@host:1000',
+    username='user',
+    password=None,
+    host='host',
+    port=1000,
+    path='',
+  )
+
 def test_prefix_tree():
   from ufs.utils.pathlib import SafePurePosixPath
   from ufs.utils.prefix_tree import create_prefix_tree_from_paths, search_prefix_tree, list_prefix_tree
