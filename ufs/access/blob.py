40acec8a1e20549d25e7830e36ff7a710c90d411
Daniel Clarke <u8sand@gmail.com>
2023-07-05 12:42:53 -0400

    [access.blob]: Make blob DRS compatible

--- ufs/access/blob.py
+++ ufs/access/blob.py
@@ -1,14 +1,24 @@
 import os
+import re
 import sys
 import flask
 import hashlib
+import datetime as dt
 from ufs.spec import UFS
 from ufs.impl.tempdir import TemporaryDirectory
 from ufs.utils.pathlib import SafePurePosixPath
 from ufs.access.pathlib import UPath
 from ufs.access.shutil import movefile
 
-def flask_ufs_for_blob(ufs: UFS, tmpdir: UFS, *, app: flask.Flask | flask.Blueprint):
+def RFC3339(ts = None):
+  if ts is not None:
+    ts = dt.datetime.fromtimestamp(ts)
+  else:
+    ts = dt.datetime.now()
+  return ts.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+def flask_ufs_for_blob(ufs: UFS, tmpdir: UFS, *, app: flask.Flask | flask.Blueprint, public_url: str):
+  created_at = RFC3339()
   @app.post('/ufs/blob/v1/objects')
   def objects_post():
     import uuid
@@ -34,6 +44,59 @@ def flask_ufs_for_blob(ufs: UFS, tmpdir: UFS, *, app: flask.Flask | flask.Bluepr
       flask.abort(404)
     return flask.Response(ufs.cat(SafePurePosixPath(object_id)))
   #
+  @app.get('/ga4gh/drs/v1/service-info')
+  def service_info():
+    return {
+      "id": "cloud.maayanlab.ufs",
+      "name": "UFS",
+      "type": {
+        "group": "org.ga4gh",
+        "artifact": "drs",
+        "version": "1.0.0"
+      },
+      "description": "This service provides DRS capabilities for UFS.",
+      "organization": {
+        "name": "Playbook Partnership",
+        "url": "https://playbook-workflow-builder.cloud"
+      },
+      "contactUrl": "mailto:avi.maayan@mssm.edu",
+      "documentationUrl": "https://github.com/nih-cfde/playbook-partnership",
+      "createdAt": created_at,
+      "environment": "test",
+      "version": "1.0.0"
+    }
+  @app.get('/ga4gh/drs/v1/objects/<object_id>')
+  def objects_get(object_id):
+    try:
+      info = ufs.info(SafePurePosixPath(object_id))
+    except FileNotFoundError:
+      return flask.abort(404)
+    return {
+      "id": object_id,
+      "name": object_id,
+      "self_uri": f"{re.sub(r'/$', '', re.sub(r'^https', 'drs', public_url))}/{object_id}",
+      "size": info['size'],
+      "created_time": RFC3339(info.get('ctime')),
+      "checksums": [
+        {"type": "sha-256", "checksum": object_id},
+      ],
+      "access_methods": [
+        {'type': 'https', 'access_id': 'https'},
+        {'type': 'https', 'access_url': f"{public_url}/ufs/blob/v1/objects/{object_id}"},
+      ],
+    }
+  @app.get('/ga4gh/drs/v1/objects/<object_id>/access/<access_id>')
+  def objects_access_get(object_id, access_id):
+    if access_id != 'https':
+      return flask.abort(404)
+    try:
+      info = ufs.info(SafePurePosixPath(object_id))
+    except FileNotFoundError:
+      return flask.abort(404)
+    return {
+      "url": f"{public_url}/ufs/blob/v1/objects/{object_id}",
+      # "headers": {},
+    }
   return app
 
 def create_app():
@@ -50,6 +113,7 @@ def create_app():
   return flask_ufs_for_blob(
     ufs, tmpdir,
     app=flask.Flask(__name__),
+    public_url=os.environ.pop('UFS_PUBLIC_URL'),
   )
 
 if __name__ == '__main__':
