725d920333c960a4b197f7b1fb552a88aeddb8d6
Daniel Clarke <u8sand@gmail.com>
2023-07-06 10:39:23 -0400

    [access.mount]: fuse/ffuse fallback

---
+++ ufs/access/mount.py
@@ -0,0 +1,18 @@
+''' Use fuse if libfuse is available, otherwise fallback to ffuse for mounting.
+'''
+
+import contextlib
+from ufs.spec import UFS
+
+@contextlib.contextmanager
+def mount(ufs: UFS, mount_dir: str = None, readonly: bool = False):
+  try:
+    from ufs.fuse import fuse_mount as _mount
+  except ImportError:
+    import logging; logging.getLogger(__name__).warning('Install fusepy for proper fuse mounting, falling back to ffuse')
+    from ufs.ffuse import ffuse_mount as _mount
+  except OSError:
+    import logging; logging.getLogger(__name__).warning('Install libfuse for proper fuse mounting, falling back to ffuse')
+    from ufs.ffuse import ffuse_mount as _mount
+  with _mount(ufs, mount_dir, readonly=readonly) as ufs:
+    yield ufs
