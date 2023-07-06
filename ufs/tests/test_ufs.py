9c6f594ee5730216879838dd7822490fb55c95d3
Daniel Clarke <u8sand@gmail.com>
2023-07-06 11:41:29 -0400

    Add a test to verify single-file access via mapper

--- ufs/tests/test_ufs.py
+++ ufs/tests/test_ufs.py
@@ -390,3 +390,16 @@ def test_mapper():
     (mapper/'a/d').write_text('test1')
     assert (sub/'c').read_text() == 'test2'
     assert (root/'a/d').read_text() == 'test1'
+
+def test_singlefile_mapper():
+  from ufs.impl.memory import Memory
+  from ufs.impl.mapper import Mapper
+  from ufs.impl.prefix import Prefix
+  from ufs.access.pathlib import UPath
+  backing = Memory()
+  ufs = Mapper({ 'A': Prefix(backing, '/A') })
+  with ufs:
+    (UPath(backing) / 'A').write_text('a')
+    (UPath(backing) / 'B').write_text('b')
+    assert [p.name for p in UPath(ufs).iterdir()] == ['A']
+    assert (UPath(ufs)/'A').read_text() == 'a'
