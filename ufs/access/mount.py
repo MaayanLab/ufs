bece3287a1e87ea65201c18550de483a23bb46cf
Daniel Clarke <u8sand@gmail.com>
2023-07-06 11:18:59 -0400

    [access.mount]: fixup import locations

--- ufs/access/mount.py
+++ ufs/access/mount.py
@@ -7,12 +7,12 @@ from ufs.spec import UFS
 @contextlib.contextmanager
 def mount(ufs: UFS, mount_dir: str = None, readonly: bool = False):
   try:
-    from ufs.fuse import fuse_mount as _mount
+    from ufs.access.fuse import fuse_mount as _mount
   except ImportError:
     import logging; logging.getLogger(__name__).warning('Install fusepy for proper fuse mounting, falling back to ffuse')
-    from ufs.ffuse import ffuse_mount as _mount
+    from ufs.access.ffuse import ffuse_mount as _mount
   except OSError:
     import logging; logging.getLogger(__name__).warning('Install libfuse for proper fuse mounting, falling back to ffuse')
-    from ufs.ffuse import ffuse_mount as _mount
+    from ufs.access.ffuse import ffuse_mount as _mount
   with _mount(ufs, mount_dir, readonly=readonly) as ufs:
     yield ufs
