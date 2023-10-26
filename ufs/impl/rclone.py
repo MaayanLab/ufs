488b8739450f3ba268f2ad12b7e42f704323c7f2
Daniel Clarke <u8sand@gmail.com>
2023-10-26 12:00:01 -0400

    [rclone]: Move process management out of RClone into helper

--- ufs/impl/rclone.py
+++ ufs/impl/rclone.py
@@ -1,17 +1,15 @@
 ''' Access any `rclone`-supported protocol
 '''
 
-import sys
 import uuid
 import json
-import socket
 import logging
 import requests
 import functools
+import contextlib
+import typing as t
 from ufs.spec import UFS, DescriptorFromAtomicMixin, ReadableIterator
 from ufs.utils.pathlib import SafePurePosixPath
-from ufs.utils.polling import wait_for, safe_predicate
-from subprocess import Popen
 
 logger = logging.getLogger(__name__)
 
@@ -31,48 +29,71 @@ def rstrip_iter(it, rstrip):
     last = el
   yield last.rstrip(rstrip)
 
+@contextlib.contextmanager
+def serve_rclone_rcd(env: dict = {}):
+  ''' RClone operates through an `rclone rcd` server, this helper
+  can be used to ensure one is running for the duration of the context manager.
+  '''
+  import sys
+  import socket
+  import shutil
+  from subprocess import Popen
+  from ufs.utils.process import active_process
+  from ufs.utils.polling import wait_for, safe_predicate
+  rclone = shutil.which('rclone')
+  docker = shutil.which('docker')
+  assert rclone or docker, 'Failed find binary for running rclone'
+  with socket.socket() as s:
+    s.bind(('127.0.0.1', 0))
+    rclone_host, rclone_port = s.getsockname()
+  rclone_user, rclone_pass = str(uuid.uuid4()), str(uuid.uuid4())
+  if rclone:
+    proc = Popen([
+      rclone, 'rcd',
+      '--rc-addr', f"{rclone_host}:{rclone_port}",
+      '--rc-user', rclone_user,
+      '--rc-pass', rclone_pass,
+    ], env=env, stdout=sys.stdout, stderr=sys.stderr)
+  else:
+    proc = Popen([
+      docker, 'run',
+      *[arg for key in env for arg in ['-e', key]],
+      '-p', f"{rclone_port}:8080",
+      '-i', 'rclone/rclone', 'rcd',
+      '--rc-addr', f"0.0.0.0:8080",
+      '--rc-user', rclone_user,
+      '--rc-pass', rclone_pass,
+    ], stdout=sys.stdout, stderr=sys.stderr)
+  with active_process(proc):
+    rclone_config = dict(url=f"http://{rclone_host}:{rclone_port}", auth=(rclone_user, rclone_pass))
+    wait_for(functools.partial(safe_predicate, lambda: requests.post(f"{rclone_config['url']}/config/listremotes", auth=rclone_config['auth']).status_code == 200))
+    yield rclone_config
+
 class RClone(DescriptorFromAtomicMixin, UFS):
-  def __init__(self, env: dict = {}):
+  def __init__(self, url: str, auth: t.Tuple[str, str]):
     super().__init__()
-    self._env = env
+    self._url = url
+    self._auth = auth
 
   @staticmethod
-  def from_dict(*, env):
+  def from_dict(*, url, auth):
     return RClone(
-      env=env,
+      url=url,
+      auth=auth,
     )
 
   def to_dict(self):
     return dict(super().to_dict(),
-      env=self._env,
+      url=self._url,
+      auth=self._auth,
     )
-
-  def start(self):
-    if not hasattr(self, '_proc'):
-      with socket.socket() as s:
-        s.bind(('127.0.0.1', 0))
-        self._host, self._port = s.getsockname()
-      self._user, self._pass = str(uuid.uuid4()), str(uuid.uuid4())
-      self._proc = Popen([
-        'rclone', 'rcd',
-        '--rc-addr', f"{self._host}:{self._port}",
-        '--rc-user', self._user,
-        '--rc-pass', self._pass,
-      ], env=self._env, stderr=sys.stderr, stdout=sys.stdout)
-      wait_for(functools.partial(safe_predicate, lambda: requests.post(f"http://{self._host}:{self._port}/config/listremotes", auth=(self._user, self._pass)).status_code == 200))
-
-  def stop(self):
-    if hasattr(self, '_proc'):
-      self._proc.terminate()
-      self._proc.wait()
-      del self._proc
 
   def ls(self, path):
     fs, path = rclone_uri_from_path(path)
     if not fs:
       req = requests.post(
-        f"http://{self._host}:{self._port}/config/listremotes",
-        auth=(self._user, self._pass),
+        f"{self._url}/config/listremotes",
+        auth=self._auth,
       )
       if req.status_code != 200:
         raise FileNotFoundError()
@@ -81,8 +102,8 @@ class RClone(DescriptorFromAtomicMixin, UFS):
     else:
       if not path: path = ''
       req = requests.post(
-        f"http://{self._host}:{self._port}/operations/list",
-        auth=(self._user, self._pass),
+        f"{self._url}/operations/list",
+        auth=self._auth,
         params=dict(fs=fs, remote=str(path)[1:]),
       )
       if req.status_code != 200:
@@ -96,8 +117,8 @@ class RClone(DescriptorFromAtomicMixin, UFS):
       return { 'type': 'directory', 'size': 0 }
     if not path:
       req = requests.post(
-        f"http://{self._host}:{self._port}/operations/fsinfo",
-        auth=(self._user, self._pass),
+        f"{self._url}/operations/fsinfo",
+        auth=self._auth,
         params=dict(fs=fs),
       )
       if req.status_code != 200:
@@ -105,8 +126,8 @@ class RClone(DescriptorFromAtomicMixin, UFS):
       return { 'type': 'directory', 'size': 0 }
     else:
       req = requests.post(
-        f"http://{self._host}:{self._port}/operations/list",
-        auth=(self._user, self._pass),
+        f"{self._url}/operations/list",
+        auth=self._auth,
         params=dict(fs=fs, remote=str(path.parent)[1:]),
       )
       if req.status_code != 200:
@@ -136,8 +157,8 @@ class RClone(DescriptorFromAtomicMixin, UFS):
     if not fs: raise PermissionError()
     if not path: raise PermissionError()
     req = requests.post(
-      f"http://{self._host}:{self._port}/core/command",
-      auth=(self._user, self._pass),
+      f"{self._url}/core/command",
+      auth=self._auth,
       json=dict(
         command='cat',
         arg=json.dumps([fs + str(path)[1:]]),
@@ -155,8 +176,8 @@ class RClone(DescriptorFromAtomicMixin, UFS):
     if not fs: raise PermissionError()
     if not path: raise PermissionError()
     requests.post(
-      f"http://{self._host}:{self._port}/operations/uploadfile",
-      auth=(self._user, self._pass),
+      f"{self._url}/operations/uploadfile",
+      auth=self._auth,
       params=dict(fs=fs, remote=str(path.parent)[1:]),
       files={'file0': (path.name, ReadableIterator(data), 'application/octet-stream')},
     )
@@ -166,8 +187,8 @@ class RClone(DescriptorFromAtomicMixin, UFS):
     if not fs: raise PermissionError()
     if not path: raise PermissionError()
     req = requests.post(
-      f"http://{self._host}:{self._port}/operations/deletefile",
-      auth=(self._user, self._pass),
+      f"{self._url}/operations/deletefile",
+      auth=self._auth,
       params=dict(fs=fs, remote=str(path)[1:])
     )
     if req.status_code != 200:
@@ -185,8 +206,8 @@ class RClone(DescriptorFromAtomicMixin, UFS):
       else:
         raise FileExistsError()
     req = requests.post(
-      f"http://{self._host}:{self._port}/operations/mkdir",
-      auth=(self._user, self._pass),
+      f"{self._url}/operations/mkdir",
+      auth=self._auth,
       params=dict(fs=fs, remote=str(remote)[1:]),
     )
     ret = req.json()
@@ -196,8 +217,8 @@ class RClone(DescriptorFromAtomicMixin, UFS):
     if not fs: raise PermissionError()
     if not path: raise PermissionError()
     req = requests.post(
-      f"http://{self._host}:{self._port}/operations/rmdir",
-      auth=(self._user, self._pass),
+      f"{self._url}/operations/rmdir",
+      auth=self._auth,
       params=dict(fs=fs, remote=str(path)[1:]),
     )
     ret = req.json()
@@ -208,8 +229,8 @@ class RClone(DescriptorFromAtomicMixin, UFS):
     if not src_fs or not dst_fs: raise PermissionError()
     if not src_path or not dst_path: raise PermissionError()
     req = requests.post(
-      f"http://{self._host}:{self._port}/operations/copyfile",
-      auth=(self._user, self._pass),
+      f"{self._url}/operations/copyfile",
+      auth=self._auth,
       params=dict(
         srcFs=src_fs,
         srcRemote=str(src_path)[1:],
@@ -225,8 +246,8 @@ class RClone(DescriptorFromAtomicMixin, UFS):
     if not src_fs or not dst_fs: raise PermissionError()
     if not src_path or not dst_path: raise PermissionError()
     req = requests.post(
-      f"http://{self._host}:{self._port}/operations/movefile",
-      auth=(self._user, self._pass),
+      f"{self._url}/operations/movefile",
+      auth=self._auth,
       params=dict(
         srcFs=src_fs,
         srcRemote=str(src_path)[1:],
--- ufs/tests/fixtures/rclone_local.py
+++ ufs/tests/fixtures/rclone_local.py
@@ -5,28 +5,30 @@ def ufs():
     import requests
   except ImportError:
     pytest.skip('Install requests for rclone support')
-  else:
-    try:
-      import shutil
-      assert shutil.which('rclone')
-    except AssertionError:
-      pytest.skip('rclone binary not found, skipping rclone')
-    else:
-      import pathlib
-      import tempfile
-      from ufs.impl.rclone import RClone
-      from ufs.impl.prefix import Prefix
-      from ufs.impl.writecache import Writecache
-      from ufs.impl.memory import Memory
-      from ufs.impl.logger import Logger
-      with tempfile.TemporaryDirectory() as tmp:
-        tmp = pathlib.Path(tmp)
-        (tmp/'data').mkdir()
-        (tmp/'config').mkdir()
-        (tmp/'config'/'rclone.conf').touch()
-        with Writecache(Prefix(Logger(RClone(dict(
-          RCLONE_CONFIG_DIR=str(tmp/'config'),
-          RCLONE_CONFIG_LOCAL_TYPE='alias',
-          RCLONE_CONFIG_LOCAL_REMOTE=str(tmp/'data'),
-        ))), '/local'), Memory()) as ufs:
-          yield ufs
+  try:
+    import shutil
+    rclone = shutil.which('rclone')
+    docker = shutil.which('docker')
+    assert rclone or docker
+  except AssertionError:
+    pytest.skip('Failed find binary for running rclone')
+  #
+  import pathlib
+  import tempfile
+  from ufs.impl.rclone import RClone, serve_rclone_rcd
+  from ufs.impl.prefix import Prefix
+  from ufs.impl.writecache import Writecache
+  from ufs.impl.memory import Memory
+  from ufs.impl.logger import Logger
+  with tempfile.TemporaryDirectory() as tmp:
+    tmp = pathlib.Path(tmp)
+    (tmp/'data').mkdir()
+    (tmp/'config').mkdir()
+    (tmp/'config'/'rclone.conf').touch()
+    with serve_rclone_rcd(dict(
+      RCLONE_CONFIG_DIR=str(tmp/'config'),
+      RCLONE_CONFIG_LOCAL_TYPE='alias',
+      RCLONE_CONFIG_LOCAL_REMOTE=str(tmp/'data'),
+    )) as rclone_config:
+      with Writecache(Prefix(Logger(RClone(**rclone_config)), '/local'), Memory()) as ufs:
+        yield ufs
