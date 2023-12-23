bf6ef214fc37983838db7555a6b5887c582c1944
Daniel Clarke <u8sand@gmail.com>
2023-12-23 17:30:25 -0500

    Writecache+Process wrappers to fix fsspec-memory

--- ufs/tests/fixtures/fsspec_memory.py
+++ ufs/tests/fixtures/fsspec_memory.py
@@ -1,5 +1,8 @@
 def ufs():
   from ufs.impl.fsspec import FSSpec
+  from ufs.impl.memory import Memory
+  from ufs.impl.process import Process
+  from ufs.impl.writecache import Writecache
   from fsspec.implementations.memory import MemoryFileSystem
-  with FSSpec(MemoryFileSystem()) as ufs:
+  with Writecache(Process(FSSpec(MemoryFileSystem())), Memory()) as ufs:
     yield ufs
