5b0caf141f87e8378efff7bf4020f5f7c9b7769a
Daniel Clarke <u8sand@gmail.com>
2025-05-23 16:46:53 -0400

    Further simplify s3 impl

--- ufs/impl/s3.py
+++ ufs/impl/s3.py
@@ -3,14 +3,13 @@
 This is based off of s3fs from the fsspec ecosystem, but with several patches to successfully pass our test suite.
 '''
 
-import time
 import errno
 import typing as t
 from ufs.spec import UFS, FileStat
-from ufs.utils.pathlib import SafePurePosixPath_
 from ufs.impl.fsspec import FSSpec
 from s3fs import S3FileSystem
 from s3fs.core import S3FileSystem
+
 class S3(FSSpec, UFS):
   ''' FSSpec's S3 has some quirks we'll want to deal with
   '''
@@ -19,8 +18,6 @@ class S3(FSSpec, UFS):
     self._access_key = access_key
     self._secret_access_key = secret_access_key
     self._endpoint_url = endpoint_url
-    self._blankfiles: dict[SafePurePosixPath_, FileStat] = {}
-
     super().__init__(S3FileSystem(anon=anon, key=access_key, secret=secret_access_key,
                           client_kwargs=dict(endpoint_url=endpoint_url)), ttl=0)
   
@@ -46,52 +43,19 @@ class S3(FSSpec, UFS):
     '''
     if 'r' in mode:
       self.info(path)
-    elif 'w' in mode:
-      self._blankfiles[path] = {
-        'type': 'file',
-        'size': 0,
-        'atime': time.time(),
-        'ctime': time.time(),
-        'mtime': time.time(),
-      }
     return FSSpec.open(self, path, mode, size_hint=size_hint)
 
-  def write(self, fd, data):
-    ret = super().write(fd, data)
-    path, _ = self._fds[fd]
-    if path in self._blankfiles:
-      self._blankfiles[path]['size'] += ret
-    return ret
-
-  def close(self, fd):
-    path, _fh = self._fds[fd]
-    FSSpec.close(self, fd)
-    self._blankfiles.pop(path, None)
-
   def ls(self, path):
-    files = (
-      [name for name in FSSpec.ls(self, path) if name != '_']
-      + [p.name for p in self._blankfiles if p.parent == path]
-    )
+    files = [name for name in FSSpec.ls(self, path) if name != '_']
     return files
 
   def info(self, path) -> FileStat:
-    if path in self._blankfiles:
-       return self._blankfiles[path]
     if str(path).count('/') > 1:
       try:
         info = {**FSSpec.info(self, path/'_')}
         info['type']='directory'
         return info
       except FileNotFoundError: pass
-    else:
-      return {
-        'type': 'directory',
-        'size': 0,
-        'atime': 0,
-        'ctime': 0,
-        'mtime': 0,
-      }
     return FSSpec.info(self, path)
 
   def mkdir(self, path):
@@ -116,5 +80,8 @@ class S3(FSSpec, UFS):
   def rename(self, src, dst):
     ''' s3fs doesn't support rename, use the fallback
     '''
+    UFS.rename(self, src, dst)
+    self._info_cache.discard(self._path(src))
+    self._ls_cache.discard(self._path(src.parent))
+    self._info_cache.discard(self._path(dst))
     self._ls_cache.discard(self._path(dst.parent))
-    return UFS.rename(self, src, dst)
