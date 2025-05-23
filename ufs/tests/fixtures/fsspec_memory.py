b57dc410cc8d7fece50cb74331cf6d0240ccdbc4
Daniel Clarke <u8sand@gmail.com>
2025-05-23 12:06:46 -0400

    Fixup fsspec_memory tests by using temporary directory on top of fsspec memory

--- ufs/tests/fixtures/fsspec_memory.py
+++ ufs/tests/fixtures/fsspec_memory.py
@@ -1,5 +1,7 @@
 def ufs():
+  # We use a temporary in fsspec memory otherwise we clash across tests
   from ufs.impl.fsspec import FSSpec
+  from ufs.impl.tempdir import TemporaryDirectory
   from fsspec.implementations.memory import MemoryFileSystem
-  with FSSpec(MemoryFileSystem()) as ufs:
+  with TemporaryDirectory(FSSpec(MemoryFileSystem())) as ufs:
     yield ufs
