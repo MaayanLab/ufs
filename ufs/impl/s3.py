d8a0a7642ca25ed71c2abf150440e4d6b5903e42
Daniel Clarke <u8sand@gmail.com>
2025-05-23 17:07:05 -0400

    Get s3 tests passing *with ttl cache*

--- ufs/impl/s3.py
+++ ufs/impl/s3.py
@@ -13,21 +13,22 @@ from s3fs.core import S3FileSystem
 class S3(FSSpec, UFS):
   ''' FSSpec's S3 has some quirks we'll want to deal with
   '''
-  def __init__(self, anon = False, access_key = None, secret_access_key = None, endpoint_url='https://s3.amazonaws.com'):
+  def __init__(self, anon = False, access_key = None, secret_access_key = None, endpoint_url='https://s3.amazonaws.com', ttl=60):
     self._anon = anon
     self._access_key = access_key
     self._secret_access_key = secret_access_key
     self._endpoint_url = endpoint_url
     super().__init__(S3FileSystem(anon=anon, key=access_key, secret=secret_access_key,
-                          client_kwargs=dict(endpoint_url=endpoint_url)), ttl=0)
+                          client_kwargs=dict(endpoint_url=endpoint_url)), ttl=ttl)
   
   @staticmethod
-  def from_dict(*, anon, access_key, secret_access_key, endpoint_url):
+  def from_dict(*, anon, access_key, secret_access_key, endpoint_url, ttl):
     return S3(
       anon=anon,
       access_key=access_key,
       secret_access_key=secret_access_key,
       endpoint_url=endpoint_url,
+      ttl=ttl,
     )
 
   def to_dict(self):
@@ -36,6 +37,7 @@ class S3(FSSpec, UFS):
       access_key=self._access_key,
       secret_access_key=self._secret_access_key,
       endpoint_url=self._endpoint_url,
+      ttl=self._ttl,
     )
 
   def open(self, path, mode, *, size_hint: t.Optional[int] = None):
@@ -76,6 +78,11 @@ class S3(FSSpec, UFS):
     except FileNotFoundError as e: raise NotADirectoryError(str(path)) from e
     if str(path).count('/') == 1: # bucket level
       FSSpec.rmdir(self, path)
+    else:
+      # S3's rmdir doesn't work outside of bucket level but we still need to clear the cache
+      self._info_cache.discard(self._path(path))
+      self._ls_cache.discard(self._path(path))
+      self._ls_cache.discard(self._path(path.parent))
 
   def rename(self, src, dst):
     ''' s3fs doesn't support rename, use the fallback
