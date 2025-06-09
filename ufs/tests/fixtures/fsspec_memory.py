0a3a6fecfa26dda44115debb3562b61014792c42
Daniel J. B. Clarke <u8sand@gmail.com>
2025-06-09 16:37:35 -0400

    Add writecache to fsspec's memory file system

--- ufs/tests/fixtures/fsspec_memory.py
+++ ufs/tests/fixtures/fsspec_memory.py
@@ -2,6 +2,8 @@ def ufs():
   # We use a temporary in fsspec memory otherwise we clash across tests
   from ufs.impl.fsspec import FSSpec
   from ufs.impl.tempdir import TemporaryDirectory
+  from ufs.impl.memory import Memory
+  from ufs.impl.writecache import Writecache
   from fsspec.implementations.memory import MemoryFileSystem
-  with TemporaryDirectory(FSSpec(MemoryFileSystem())) as ufs:
+  with TemporaryDirectory(Writecache(FSSpec(MemoryFileSystem()), Memory())) as ufs:
     yield ufs
