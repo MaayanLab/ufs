ac5eb9eccc6bb027316803fb50680cc236186ddb
Daniel Clarke <u8sand@gmail.com>
2025-07-01 14:54:46 -0400

    Add tests to attempt mounting all fixtures

---
+++ ufs/tests/test_mountable.py
@@ -0,0 +1,20 @@
+import pytest
+from ufs.spec import UFS
+
+from ufs.tests.fixtures import ufs
+
+@pytest.mark.parametrize('fuse', [True, False])
+def test_mountable(ufs: UFS, fuse):
+  from ufs.access.mount import mount
+  from ufs.access.pathlib import UPath
+  with mount(ufs, fuse=fuse) as mount_dir:
+    (mount_dir/'test').mkdir(parents=True)
+    with (mount_dir/'test'/'a').open('w+') as fw:
+      fw.write('Hello World!')
+      fw.seek(0)
+      fw.write('Hi')
+    import time; time.sleep(0.5)
+  upath = UPath(ufs)
+  assert [p.name for p in upath.iterdir()] == ['test']
+  assert (upath/'test').is_dir()
+  assert (upath/'test'/'a').read_text() == 'Hillo World!'
