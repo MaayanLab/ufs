d3d5af2734e091e2e2d115c51d578c0d99ff3593
Daniel Clarke <u8sand@gmail.com>
2025-07-07 17:13:48 -0400

    All tests passing on linux

--- ufs/tests/test_mountable.py
+++ ufs/tests/test_mountable.py
@@ -1,6 +1,7 @@
 import pytest
 import shutil
 from ufs.spec import UFS, AccessScope
+from ufs.impl.sync import Sync
 from ufs.impl.logger import Logger
 
 from ufs.tests.fixtures import ufs
@@ -10,7 +11,7 @@ def test_mountable(ufs: UFS, fuse):
   from ufs.access.pathlib import UPath
   if fuse and ufs.scope().value < AccessScope.system.value:
     pytest.skip('UFS store access scope not compatible with fuse')
-  with mount(Logger(ufs), fuse=fuse) as mount_dir:
+  with mount(Logger(Sync(ufs)), fuse=fuse) as mount_dir:
     # verify we can do things in the mount
     path = mount_dir / ('fuse' if fuse else 'ffuse')
     path.mkdir()
@@ -33,22 +34,18 @@ def test_mountable(ufs: UFS, fuse):
       fa.write('\n!')
     with pytest.raises(FileNotFoundError): (path/'A').read_text()
     # native shutil copyfile should work
-    # shutil.copyfile(path/'B', path/'A')
-    # native cp should work
-    import subprocess
-    assert subprocess.check_call(['cat', str(path/'B')]) == 0
-    assert subprocess.check_call(['cp', str(path/'B'), str(path/'A')]) == 0
+    shutil.copyfile(path/'B', path/'A')
     # is the content the same as what we wrote after the rename?
     with (path/'B').open('r') as fr:
       fr = iter(fr)
       assert next(fr) == 'hello world!\n'
       assert next(fr) == '!'
       with pytest.raises(StopIteration): next(fr)
-    assert [p.name for p in path.iterdir()] == ['A', 'B']
+    assert {p.name for p in path.iterdir()} == {'A', 'B'}
     # can we remove a file?
     (path/'B').unlink()
     assert not (path/'B').exists()
   # make sure the changes have also propagated to the underlying store
   upath = UPath(ufs)/ ('fuse' if fuse else 'ffuse')
-  assert [p.name for p in upath.iterdir()] == ['A']
+  assert {p.name for p in upath.iterdir()} == {'A'}
   assert (upath/'A').read_text() == 'hello world!\n!'
--- ufs/tests/test_path.py
+++ ufs/tests/test_path.py
@@ -38,30 +38,31 @@ def test_path(ufs):
 async def test_async_path(ufs):
   ''' Actually test that filesystem ops work as expected
   '''
-  path = AsyncUPath(Async(ufs)) / 'pathlib'
-  await path.mkdir(parents=True, exist_ok=True)
-  assert await path.is_dir()
-  async with await (path/'A').open('w') as fw:
+  async with Async(ufs) as ufs:
+    path = AsyncUPath(ufs) / 'pathlib'
+    await path.mkdir(parents=True, exist_ok=True)
+    assert await path.is_dir()
+    async with await (path/'A').open('w') as fw:
+      assert await (path/'A').is_file()
+      await fw.write('Hello World!')
     assert await (path/'A').is_file()
-    await fw.write('Hello World!')
-  assert await (path/'A').is_file()
-  async with await (path/'A').open('r+') as fh:
-    assert await fh.read() == 'Hello World!'
-    await fh.seek(0)
-    await fh.write('h')
-    await fh.seek(6)
-    await fh.write('w')
-    await fh.seek(0)
-    assert await fh.read() == 'hello world!'
-  await (path/'A').rename(path/'B')
-  async with await (path/'B').open('a') as fa:
-    await fa.write('\n!')
-  with pytest.raises(FileNotFoundError): await (path/'A').read_text()
-  async with await (path/'B').open('r') as fr:
-    fr = aiter(fr)
-    assert await fr.__anext__() == 'hello world!\n'
-    assert await fr.__anext__() == '!'
-    with pytest.raises(StopAsyncIteration): await fr.__anext__()
-  assert [p.name async for p in path.iterdir()] == ['B']
-  await (path/'B').unlink()
-  assert not await (path/'B').exists()
+    async with await (path/'A').open('r+') as fh:
+      assert await fh.read() == 'Hello World!'
+      await fh.seek(0)
+      await fh.write('h')
+      await fh.seek(6)
+      await fh.write('w')
+      await fh.seek(0)
+      assert await fh.read() == 'hello world!'
+    await (path/'A').rename(path/'B')
+    async with await (path/'B').open('a') as fa:
+      await fa.write('\n!')
+    with pytest.raises(FileNotFoundError): await (path/'A').read_text()
+    async with await (path/'B').open('r') as fr:
+      fr = aiter(fr)
+      assert await fr.__anext__() == 'hello world!\n'
+      assert await fr.__anext__() == '!'
+      with pytest.raises(StopAsyncIteration): await fr.__anext__()
+    assert [p.name async for p in path.iterdir()] == ['B']
+    await (path/'B').unlink()
+    assert not await (path/'B').exists()
